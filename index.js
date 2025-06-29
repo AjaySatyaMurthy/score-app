5bd5bac311ab17f4896132b29453de46dfa0bcfb
AjaySatyaMurthy <157121142+AjaySatyaMurthy@users.noreply.github.com>
2025-06-29 23:42:02 +0530

    all files of score app

---
+++ index.js
@@ -0,0 +1,51 @@
+let guest = document.getElementById("guestScore")
+
+let home = document.getElementById("homeScore")
+
+let homeNumber = 0
+
+let guestNumber = 0
+
+// homeScore //
+
+function homeScoreOne() {
+    homeNumber += 1
+    home.textContent = homeNumber
+}
+
+function homeScoreTwo() {
+    homeNumber += 2
+    home.textContent = homeNumber
+}
+
+function homeScoreThree() {
+    homeNumber += 3
+    home.textContent = homeNumber
+}
+
+//guest score //
+
+function guestScoreOne() {
+    guestNumber += 1
+    guest.textContent = guestNumber
+}
+
+function guestScoreTwo() {
+    guestNumber += 2
+    guest.textContent = guestNumber
+}
+
+function guestScoreThree() {
+    guestNumber += 3
+    guest.textContent = guestNumber
+}
+
+// reset button
+
+function resetHomeBtn() {
+    home.textContent = 0
+}
+
+function resetGuestBtn() {
+    guest.textContent = 0
+}
